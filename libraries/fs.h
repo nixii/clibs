d864018ac350e04aacb2a5ebb3863331b206e823
nixii <161391079+nixii@users.noreply.github.com>
2025-12-04 21:50:28 -0800

    mkdir_all works for unresolved paths

--- libraries/fs.h
+++ libraries/fs.h
@@ -5,6 +5,9 @@
 #ifndef FS_H
 #define FS_H
 
+// Resolve a path
+char * fs_resolved_path(const char *path);
+
 // Check if a path exsits. Returns true(1) if it exists and false(0) if it doesn't.
 int fs_exists(const char *path);
 
@@ -16,6 +19,18 @@ int fs_is_file(const char *path);
 
 // Create a directory safely. Returns an error code. If the directory already exists, it still returns 0, but you can check errno.
 int fs_mkdir(const char *path);
+
+// Create every directory recursively. However, this does use a lot more memory (~1-4 KiB of RAM when being run). It is recommended to use fs_mkdir if that matters.
+int fs_mkdir_all(const char *path);
+
+
+// Platform-specific constants
+#ifdef _WIN32
+	
+	#define __FS_MAX_PATH_BUFFER MAX_PATH
+#else
+	#define __FS_MAX_PATH_BUFFER PATH_MAX
+#endif
 
 #endif
 
@@ -32,7 +47,9 @@ int fs_mkdir(const char *path);
 	#include <windows.h>
 	#include <direct.h>
 	#define __FS_MKDIR(dir) _mkdir(dir)
+	#define __FS_RESOLVEPATH()
 #else
+	#include <sys/syslimits.h>
 	#include <sys/stat.h>
 	#include <sys/types.h>
 	#include <unistd.h>
@@ -42,6 +59,10 @@ int fs_mkdir(const char *path);
 // General include statements
 #include <string.h>
 #include <stdio.h>
+#include <errno.h>
+
+// Externals
+extern int errno;
 
 // Return whether or not a path exists
 int fs_exists(const char *path) {
@@ -75,6 +96,34 @@ int fs_is_file(const char *path) {
 #endif
 }
 
+// Create a directory
+int fs_mkdir(const char *path) {
+	int res = __FS_MKDIR(path);
+	if (res != 0)
+		if (res == EEXIST)
+			return 0;
+	return res;
+}
 
+// Recursively create directories
+int fs_mkdir_all(const char *path) {
+	char *npath = fs_resolved_path(path);
+	char buf[__FS_MAX_PATH_BUFFER + 1];
+
+	int idx = 0;
+	while (npath[idx]) {
+		if (npath[idx] == '/' || npath[idx] == '\\') {
+			memcpy(&buf, npath, idx + 1);
+			buf[idx+1] = '\0';
+			int res = fs_mkdir(buf);
+			printf("%s\n", buf);
+			if (res != 0)
+				return res;
+		}
+		++idx;
+	}
+
+	return 0;
+}
 
 #endif
